7e1285a64135d2c2356f36a7234b71addff4b89b
Azat <46015824+serikovskii@users.noreply.github.com>
2019-03-12 15:52:07 +0600

    dz

---
+++ HomeWorkIndex/Index.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+1.	Создать индексатор для одномерного массива который 
+при установке значения 
+будет возводить в квадрат передаваемое 
+значение переменной и устанавливать
+его для указанного индекса. 
+При получении элемента массива по индексу будет 
+возвращаться его текущее значение 
+*/
+namespace HomeWorkIndex
+{
+    public class Index
+    {
+        int[] array;
+        int size = 5;
+        public Index()
+        {
+            array = new int[size];
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value*value;
+            }
+        }
+    }
+}
---
+++ HomeWorkIndex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HomeWorkIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Index array = new Index();
+            array[3] = 6;
+            Console.WriteLine(array[3]);
+
+        }
+    }
+}
